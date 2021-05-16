90e934785c354c7359d75b6069d76d5bdc7d55e9
Aleksandr Gorokhov <agorokhov94@gmail.com>
2021-05-16 10:53:50 +0300

    Add oh-my-zsh installation

--- macreset.sh
+++ macreset.sh
@@ -2,3 +2,7 @@
 
 echo "### Installing Homebrew ###"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "### Installing oh-my-zsh ###"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
