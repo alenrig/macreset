241f8016d7bdb5bce343032ce4bb6f1707e24c5e
Aleksandr Gorokhov <agorokhov94@gmail.com>
2021-05-16 10:57:44 +0300

    Add packages installation loop

--- macreset.sh
+++ macreset.sh
@@ -9,3 +9,17 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "### Update Packages ###"
 brew update && brew outdated && brew upgrade && brew cleanup
 
+packages=(
+    ansible docker golang nvm pyenv pipenv nvim \
+    minikube kubectl terraform vagrant virtualbox wget \
+    gotomeeting vk-messenger telegram-desktop zoomus \
+    visual-studio-code \
+    alfred notion teamviewier transmission musescore \
+    loom copyclip cleanshot youtube-dl
+)
+
+echo "### Install Packages ###"
+for package in ${packages[@]}
+do
+    brew install $package
+done
