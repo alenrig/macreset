5616d0636236493a1e230535b4ab76edc4fd67e7
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-03-29 19:01:30 +0300

    Start

--- macreset.sh
+++ macreset.sh
@@ -1,48 +1,29 @@
 #!/bin/bash
 
-echo "### Installing Homebrew ###"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "### Install Apple's Command Line Tools ###"
+xcode-select --install
 
-echo "### Installing oh-my-zsh ###"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "### Update Packages ###"
-brew update && brew outdated && brew upgrade && brew cleanup
+echo "### Install Ansible ###"
+export PATH="$HOME/Library/Python/3.9/bin:/opt/homebrew/bin:$PATH"
+sudo pip3 install --upgrade pip
+pip3 install ansible
 
-packages=(
-    ansible docker golang nvm pyenv pipenv nvim \
-    minikube kubectl openshift-cli awscli terraform wget \
-    telegram-desktop zoom \
-    visual-studio-code iterm2 \
-    notion transmission musescore \
-    copyclip youtube-dl monitorcontrol \
-    slack middleclick balenaetcher wireguard \
-    fantastical google-drive \
-    maccy vanilla shottr rectangle iina
-)
+echo "### Install Ansible Requirements ###"
+ansible-galaxy install -r requirements.yaml
 
-echo "### Install Packages ###"
-for package in ${packages[@]}
-do
-    brew install $package
-done
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+echo "### Run Ansible Playbook ###"
+ansible-playbook main.yaml --ask-become-pass
 
-echo "### Update Configs ###"
-cp dots/.zshrc $HOME
-cp dots/.vimrc $HOME
 
-mkdir -p $HOME/.config/nvim
-cp dots/init.vim /$HOME/.config/nvim
+# echo "### Update vim ###"
+# mkdir -p $HOME/.vim/bundle
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "### Update vim ###"
-mkdir -p $HOME/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# echo -e "### Update zsh ###"
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo -e "### Update zsh ###"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-mkdir -p .oh-my-zsh/custom/themes
-git clone https://github.com/dracula/zsh.git $HOME/zsh-dracula-theme
-cp -R $HOME/zsh-dracula-theme/{dracula.zsh-theme,lib} $HOME/.oh-my-zsh/custom/themes
-rm -rf $HOME/zsh-dracula-theme
+# mkdir -p .oh-my-zsh/custom/themes
+# git clone https://github.com/dracula/zsh.git $HOME/zsh-dracula-theme
+# cp -R $HOME/zsh-dracula-theme/{dracula.zsh-theme,lib} $HOME/.oh-my-zsh/custom/themes
+# rm -rf $HOME/zsh-dracula-theme
