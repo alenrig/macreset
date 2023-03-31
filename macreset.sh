5c6417d8b67030767d25a8f84a1ee7d75b375f5f
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-03-31 11:44:25 +0300

    Delete old commands

--- macreset.sh
+++ macreset.sh
@@ -13,15 +13,3 @@ ansible-galaxy install -r requirements.yml
 
 echo "### Run Ansible Playbook ###"
 ansible-playbook main.yml --ask-become-pass
-
-# echo "### Update vim ###"
-# mkdir -p $HOME/.vim/bundle
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# echo -e "### Update zsh ###"
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# mkdir -p .oh-my-zsh/custom/themes
-# git clone https://github.com/dracula/zsh.git $HOME/zsh-dracula-theme
-# cp -R $HOME/zsh-dracula-theme/{dracula.zsh-theme,lib} $HOME/.oh-my-zsh/custom/themes
-# rm -rf $HOME/zsh-dracula-theme
