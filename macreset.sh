bfffb48807b31ec22767d3de6e30080742e23c64
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-12-15 20:03:10 +0300

    Update start script

--- macreset.sh
+++ macreset.sh
@@ -3,13 +3,13 @@
 echo "### Install Apple's Command Line Tools ###"
 xcode-select --install
 
-echo "### Install Ansible ###"
+echo "### Install Pipx ###"
 python3 -m venv $HOME/.local --system-site-packages
 $HOME/.local/bin/pip install --upgrade pip
 $HOME/.local/bin/pip install pipx
-$HOME/.local/bin/pipx install ansible
-$HOME/.local/bin/pipx install uv
 
+echo "### Install Ansible ###"
+$HOME/.local/bin/pipx install ansible
 echo "### Install Ansible Requirements ###"
 $HOME/.local/pipx/venvs/ansible/bin/ansible-galaxy install -r requirements.yml
 
