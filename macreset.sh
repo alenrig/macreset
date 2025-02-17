61333dd247364352ac3c5eb86dd672d964f83685
Aleksandr Gorokhov <agorokhov94@gmail.com>
2025-02-17 22:21:54 +0300

    Add shellcheck and lint script

--- macreset.sh
+++ macreset.sh
@@ -4,14 +4,14 @@ echo "### Install Apple's Command Line Tools ###"
 xcode-select --install
 
 echo "### Install Pipx ###"
-python3 -m venv $HOME/.local --system-site-packages
-$HOME/.local/bin/pip install --upgrade pip
-$HOME/.local/bin/pip install pipx
+python3 -m venv "$HOME"/.local --system-site-packages
+"$HOME"/.local/bin/pip install --upgrade pip
+"$HOME"/.local/bin/pip install pipx
 
 echo "### Install Ansible ###"
-$HOME/.local/bin/pipx install ansible
+"$HOME"/.local/bin/pipx install ansible
 echo "### Install Ansible Requirements ###"
-$HOME/.local/pipx/venvs/ansible/bin/ansible-galaxy install -r requirements.yml
+"$HOME"/.local/pipx/venvs/ansible/bin/ansible-galaxy install -r requirements.yml
 
 echo "### Run Ansible Playbook ###"
-$HOME/.local/pipx/venvs/ansible/bin/ansible-playbook main.yml --ask-become-pass
+"$HOME"/.local/pipx/venvs/ansible/bin/ansible-playbook main.yml --ask-become-pass
